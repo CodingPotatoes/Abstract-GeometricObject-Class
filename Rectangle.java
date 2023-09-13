5eb8646628a44fc4ef1ec7815337d2a98160bd65
CodingPotatoes <williamernestarcher@gmail.com>
2023-09-13 14:19:42 -0400

    Creating an abstract class and using it

---
+++ Rectangle.java
@@ -0,0 +1,52 @@
+public class Rectangle extends GeometricObject{
+
+    private double width;
+    private double height;
+
+    public Rectangle(){
+        
+    }
+
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea(){
+        return width * height;
+    }
+
+    public double getPerimeter(){
+        return 2 * (width + height);
+    }
+    
+
+    /**
+     * @return double return the width
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    /**
+     * @return double return the height
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+}
